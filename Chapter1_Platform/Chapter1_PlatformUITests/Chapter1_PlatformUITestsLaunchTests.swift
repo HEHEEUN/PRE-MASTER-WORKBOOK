d51bdea6b091d4930d761700764989ed0afdeb22
{github HEHEEUN} <{heheeun13@gmail.com}>
2023-10-06 15:21:10 +0900

    Feat: Chapter1 Storyboard 제작

---
+++ Chapter1_Platform/Chapter1_PlatformUITests/Chapter1_PlatformUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Chapter1_PlatformUITestsLaunchTests.swift
+//  Chapter1_PlatformUITests
+//
+//  Created by 김희은 on 10/6/23.
+//
+
+import XCTest
+
+final class Chapter1_PlatformUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
